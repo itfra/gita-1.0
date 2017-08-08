d394a0ffa25a588a66448ad4b831c927c628e20d
Francesco <Francesco@LAPTOP-I984LK08>
2017-08-08 14:51:48 +0200

    Pakage Partecipanti

---
+++ src/Partecipanti/CertificatoSRC.java
@@ -0,0 +1,17 @@
+package Partecipanti;
+
+
+import java.io.File;
+import javafx.scene.chart.PieChart.Data;
+
+public class CertificatoSRC {
+
+	Data data;
+	File certificato;
+	
+	Data getdata(){
+		return data;
+	}
+	
+		
+}
---
+++ src/Partecipanti/Partecipante.java
@@ -0,0 +1,13 @@
+package Partecipanti;
+
+public class Partecipante {
+	String codicefiscale;
+	String indirizzo; 
+	String annoNascita;
+	String sesso;
+	String tesseraSanitaria;
+	CertificatoSRC certificato;
+	String nome;
+	String cognome;
+	String email;
+}
